f983f79e8b028332683c6256953bdf84ae0140d5
Joe Lee <whatjoedoing@gmail.com>
2021-10-14 18:56:09 -0700

    Complete bare_minimum/callbackReview.js exercises

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,7 +6,30 @@ var fs = require('fs');
 var request = require('needle');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = (filePath, callback) => {
+  fs.readFile(filePath, 'utf8', (err, content) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      var splitted = content.split('\n');
+      callback(null, splitted[0]);
+    }
+  });
+
+
+  // if (err) {
+  //   console.log(err);
+  // } else {
+  //   fs.readFile(filePath, (err, content) => {
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       callback(null, content);
+  //     }
+  //   });
+  // }
+
+
   // TODO
 };
 
